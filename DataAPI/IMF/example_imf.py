d98d7e60f6e08d88ec2564e54c4ec9b6114a90b0
Gustavo Amarante <gustavo.amarante@bwgi.com.br>
2018-08-02 18:08:43 -0200

    New DataAPI class
    IMF class, example file and README file (as a jupyter notebook)

---
+++ DataAPI/IMF/example_imf.py
@@ -0,0 +1,17 @@
+from DataAPI import IMF
+import matplotlib.pyplot as plt
+
+imf = IMF()
+
+# df = imf.dataflow()
+# dim_code, dim_codedict = imf.data_structure('DOT')
+
+query_filter = {'CL_FREQ': 'A',
+                'CL_AREA_DOT': 'BR',
+                'CL_INDICATOR_DOT': 'TXG_FOB_USD',
+                'CL_COUNTERPART_AREA_DOT': 'US'}
+
+df = imf.compact_data('DOT', query_filter, 'BR exports to US')
+
+df.plot()
+plt.show()
