a47002c457ccbc8aeaf8770c04b169e98bd16074
Gustavo Amarante <gustavo.amarante@bwgi.com.br>
2018-10-26 11:35:46 -0200

    progress on Bloomberg API wrapper

--- fhdataapi/BBG/example_BBG.py
+++ fhdataapi/BBG/example_BBG.py
@@ -1,9 +1,12 @@
 from fhdataapi import BBG
 import pandas as pd
+import numpy as np
 
 start_date = pd.to_datetime('30-mar-2015')
 end_date = pd.to_datetime('today')
 
 bbg = BBG()
 
-bbg.fetch_series()
+response = bbg.fetch_series(securities='BRL Curncy', fields='PX_LAST', startdate=start_date, enddate=end_date)
+
+print(response)
--- fhdataapi/BBG/getbbgdata.py
+++ fhdataapi/BBG/getbbgdata.py
@@ -10,8 +10,9 @@ class BBG(object):
         self.options = BBG._parse_cmd_line()
 
     # TODO Rename the arguments
-    def fetch_series(self, securities, fields, startdate, enddate, period="DAILY", calendar="ACTUAL", FX='', fperiod='',
-                     verbose=False):
+    def fetch_series(self, securities, fields, startdate, enddate, period="DAILY", calendar="ACTUAL", fx=None,
+                     fperiod=None, verbose=False):
+
         startdate = BBG._assert_date_type(startdate)
         enddate = BBG._assert_date_type(enddate)
 
@@ -29,7 +30,98 @@ class BBG(object):
         if not session.start():
             raise ConnectionError("Failed to start session")
 
-        # PAREI NA PARTE QUE TENHO QUE PEGAR OS DADOS HISTORICOS
+        try:
+            if not session.openService("//blp/refdata"):
+                raise ConnectionError("Failed to open //blp/refdat")
+
+            # Obtain the previously opened service
+            refdata_service = session.getService("//blp/refdata")
+
+            # Create and fill the request for historical data
+            request = refdata_service.createRequest("HistoricalDataRequest")
+
+            # grab securities
+            if type(securities) is list:
+                for sec in securities:
+                    request.getElement("securities").appendValue(sec)
+            else:
+                request.getElement("securities").appendValue(securities)
+
+            # grab fields
+            if type(fields) is list:
+                for f in fields:
+                    request.getElement("fields").appendValue(f)
+            else:
+                request.getElement("fields").appendValue(fields)
+
+            request.set("periodicityAdjustment", calendar)
+            request.set("periodicitySelection", period)
+            request.set("startDate", bbg_start_date)
+            request.set("endDate", bbg_end_date)
+            request.set("maxDataPoints", 30000)
+
+            if not (fx is None):
+                request.set("currency", fx)
+
+            if not (fperiod is None):
+                overrides_bdh = request.getElement("overrides")
+                override1_bdh = overrides_bdh.appendElement()
+                override1_bdh.setElement("fieldId", "BEST_FPERIOD_OVERRIDE")
+                override1_bdh.setElement("value", fperiod)
+
+            if verbose:
+                print("Sending Request:", request.getElement("date").getValue())
+
+            # send request
+            session.sendRequest(request)
+
+            # process received response
+            results = {}
+
+            while True:
+                ev = session.nextEvent(500)
+
+                for msg in ev:
+
+                    if verbose:
+                        print(msg)
+
+                    if msg.messageType().__str__() == "HistoricalDataResponse":
+                        sec_data = msg.getElement("securityData")
+                        sec_name = sec_data.getElement("security").getValue()
+                        field_data = sec_data.getElement("fieldData")
+
+                        if type(fields) is list:
+
+                            results[sec_name] = {}
+
+                            for day in range(field_data.numValues()):
+
+                                fld = field_data.getValue(day)
+
+                                for fld_i in fields:
+                                    if fld.hasElement(fld_i):
+                                        results[sec_name]\
+                                            .setdefault(fld_i, []).append([fld.getElement("date").getValue(),
+                                                                           fld.getElement(fld_i).getValue()])
+                        else:
+                            results[sec_name] = []
+                            for day_i in range(field_data.numValues()):
+                                fld = field_data.getValue(day_i)
+                                results[sec_name].append([
+                                    fld.getElement("date").getValue(),
+                                    fld.getElement(fields).getValue()])
+
+                if ev.eventType() == blpapi.Event.RESPONSE:  # Response completly received, break out of the loop
+                    break
+
+        finally:
+            session.stop()
+
+        if type(securities) is list:
+            return results
+        else:
+            return results[securities]
 
     @staticmethod
     def _parse_cmd_line():
@@ -76,5 +168,6 @@ class BBG(object):
 * Test start_date > end_date
 * test for different types of dates
 * write documentation
+    - no more than 30k point per request
 * write README
 """
