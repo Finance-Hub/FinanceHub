beb122bdacfc3c662a441aa458f7352af70b52b7
gsoares <gustavo.soares@bwgi.com.br>
2018-12-11 16:01:57 -0300

    This code creates a a very tracker for an FX carry trade using Bloomberg data. There is a mistake in it. I'm trying to figure it out what the problem is.

--- fhdataapi/BBG/build_fx_tracker.py
+++ fhdataapi/BBG/build_fx_tracker.py
@@ -3,9 +3,10 @@ import pandas as pd
 from datetime import date, timedelta
 from pandas.tseries.offsets import BDay
 import matplotlib.pyplot as plt
-import math
+import os
 import numpy as np
 
+writer = pd.ExcelWriter(os.path.join(r'G:\GS' ,'fx_tracer.xlsx'))
 
 bbg = BBG()
 
@@ -28,7 +29,9 @@ bbg_raw_data_df = bbg.fetch_series(securities=tickers,
 
 bbg_raw_data_df.columns = ['spot','1w','1m','2m']
 
-day_count_dc = [0,7,30,60] #these are the number of days for spot, 1 week, 1 month, and 2 month
+bbg_raw_data_df.to_excel(writer,'bbg_data')
+
+day_count_dc = [0,7,31,62] #these are the number of days for spot, 1 week, 1 month, and 2 month
 
 #this is going to store the settlement dates and the outright forwards
 value_dates = pd.DataFrame(index=bbg_raw_data_df.index,columns=bbg_raw_data_df.columns)
@@ -41,13 +44,17 @@ for i in range(len(day_count_dc)):
     else:
         forwards[forwards.columns[i]] = bbg_raw_data_df['spot']
 
+forwards = forwards.fillna(method='ffill')
+
+value_dates.to_excel(writer,'value_dates')
+
 #this is going to store the day count between the foward and the spot settlement dates
 day_counts_df = pd.DataFrame(index=value_dates.index,columns=['spot'],data=0)
 for i in value_dates.columns[1:]:
     day_counts_df[i] = [(a - b).days for a, b in zip(value_dates[i], value_dates['spot'])]
 
 #We are going to build a trading stratey in which we every day trade a fraction of the notional on a 1 month forward
-number_of_trades = 21 #so, we get a strip of forwards
+number_of_trades = 20 #so, we get a strip of forwards
 how_much_pct_of_notional_to_trade_per_day = 1./float(number_of_trades)
 
 pnl = pd.Series(index=range(number_of_trades), data=0.)
@@ -58,14 +65,16 @@ notionals = pd.Series(index=range(number_of_trades), data=0.)
 
 #this is going to store the maturity of the foward being traded
 days_to_mat_new_position = pd.Series(index=value_dates.index[:-number_of_trades],
-                                     data=[(a-b).days for a,b in zip(value_dates['1m'].iloc[number_of_trades:],
-                                                                     value_dates['1m'].iloc[:-number_of_trades])])
+                                     data=[(a-b).days for a,b in zip(value_dates['spot'].iloc[number_of_trades:],
+                                                                     value_dates['spot'].iloc[:-number_of_trades])])
 
 #start the tracker index with 100 notional
 tracker_index = pd.Series(index=value_dates.index[:-number_of_trades])
 index_start_date = tracker_index.index[0]
 tracker_index.iloc[0] = 100.
 
+pnl_df = pd.DataFrame()
+list_of_dates_to_delete = []
 for d,d_minus_1 in zip(tracker_index.index[1:],tracker_index.index[:-1]):
     # move all trades down one position
     fwd_day_count.iloc[1:] = fwd_day_count.iloc[:-1].values
@@ -75,10 +84,12 @@ for d,d_minus_1 in zip(tracker_index.index[1:],tracker_index.index[:-1]):
     # insert the data for the new trade
     fwd_day_count.iloc[0] = days_to_mat_new_position[d_minus_1] -\
                             (value_dates['spot'][d]-value_dates['spot'][d_minus_1]).days
+
     notionals.iloc[0] = tracker_index[d_minus_1]*how_much_pct_of_notional_to_trade_per_day
-    fwd_strikes.iloc[0] = np.interp(float(days_to_mat_new_position[d]),
-                                    list(day_counts_df.loc[d].astype(float).values),
-                                    list(forwards.loc[d].values))
+
+    fwd_strikes.iloc[0] = np.interp(float(days_to_mat_new_position[d_minus_1]),
+                                    list(day_counts_df.loc[d_minus_1].astype(float).values),
+                                    list(forwards.loc[d_minus_1].values))
 
     # mark to market the forwards that we are holding
     fwd_mtm = pd.Series(index=fwd_strikes.index,
@@ -86,16 +97,23 @@ for d,d_minus_1 in zip(tracker_index.index[1:],tracker_index.index[:-1]):
                         list(day_counts_df.loc[d].astype(float).values),
                         list(forwards.loc[d].values)))
 
+
     # this is the pnl since the forwards were struke yesterday, excluding the trade that was closed out
     # the pnl of the trade done today is zero
     previous_pnl_sum = pnl.iloc[:-1].sum()
 
     # this is the pnl since the forwards were struke
-    pnl = notionals*(fwd_mtm/fwd_strikes-1.)
+    if currency in ['JPY']:
+        pnl = -notionals * (fwd_mtm - fwd_strikes) / fwd_mtm #these are for the cases where the currency is quoted like USDXXX
+    else:
+        pnl = notionals*(fwd_mtm/fwd_strikes-1.) #these are for the cases where the currency is quoted like XXXUSD
 
+
+    pnl_df = pd.concat([pnl_df,pnl.to_frame(d)],join='outer',axis=1)
     todays_pnl = (pnl.sum() - previous_pnl_sum) #this is how much money was made today
     tracker_index[d] = tracker_index[d_minus_1] + todays_pnl #acumulate the pnl in the tracker
 
+pnl_df.transpose().to_excel(writer,'pnl')
 
 bbg_carry_index = bbg.fetch_series(securities = currency + 'USDCR CMPN Curncy',
                                fields='PX_LAST',
@@ -105,11 +123,17 @@ bbg_carry_index.columns = ['bbg_tracker']
 
 spot_index = pd.Series(index=tracker_index.index)
 spot_index.iloc[0] = tracker_index.iloc[0]
+bbg_tracker = pd.Series(index=tracker_index.index)
+bbg_tracker.iloc[0] = tracker_index.iloc[0]
+
 for d in spot_index.index[1:]:
     spot_ret = bbg_raw_data_df['spot'].loc[:d].iloc[-1] / bbg_raw_data_df['spot'].loc[:d].iloc[-2]
     spot_index[d] = spot_index.loc[:d].iloc[-2]*(spot_ret)
+    bbg_tracker_ret = bbg_carry_index['bbg_tracker'].loc[:d].iloc[-1] / bbg_carry_index['bbg_tracker'].loc[:d].iloc[-2]
+    bbg_tracker[d] = bbg_tracker.loc[:d].iloc[-2]*(bbg_tracker_ret)
 
-df = pd.concat([bbg_carry_index,
+
+df = pd.concat([bbg_tracker.to_frame('bbg_tracker'),
                 spot_index.to_frame('spot_index'),
                 tracker_index.to_frame('my_tracker')],
                join='outer',axis=1,sort=True).fillna(method='ffill').dropna()
@@ -117,7 +141,7 @@ df = pd.concat([bbg_carry_index,
 df.plot()
 plt.show()
 
-
+writer.save()
 
 
 
