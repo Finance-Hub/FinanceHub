164c095a0149c647597010ab9c195df0941aa14e
Gustavo Amarante <gustavo.amarante@bwgi.com.br>
2018-12-13 10:49:31 -0300

    progress on the new FX tracker

--- fhtrackers/FX/example_fx_tracker.py
+++ fhtrackers/FX/example_fx_tracker.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.interpolate import interp1d
 import pandas as pd
 from pandas.tseries.offsets import BDay
 from datetime import timedelta
@@ -58,5 +60,49 @@ for mat in dict_dc.keys():
 # Percentage of notional to trade each day
 pct_notional = 1 / float(n_trades)
 
-shit = 1
+# holds necessary information for calculations
+df_calc = pd.DataFrame(index=range(n_trades),
+                       columns=['pnl', 'fwd mtm', 'fwd strike', 'fwd day count', 'notionals'])
 
+# maturity of the forward being traded
+days_mat_new_position = (df_value_dates['spot'] - df_value_dates['spot'].shift(n_trades)).shift(-n_trades)
+
+# start the tracker with 100 notional
+df_tracker = pd.DataFrame(index=df_bbg.index,
+                          columns=['index'],
+                          data=100)
+
+for d, dm1 in zip(df_tracker.index[1:], df_tracker.index[:-1]):
+
+    # move trades down by 1 position
+    df_calc['fwd day count'] = df_calc['fwd day count'].shift(1)
+    df_calc['notionals'] = df_calc['notionals'].shift(1)
+    df_calc['fwd strike'] = df_calc['fwd strike'].shift(1)
+
+    # add values for the new position
+    df_calc['fwd day count'].loc[0] = days_mat_new_position[dm1].days - (df_value_dates['spot'][d] - df_value_dates['spot'][dm1]).days
+
+    df_calc['notionals'].loc[0] = df_tracker.loc[dm1, 'index'] * pct_notional  # each of the n_trades positions has the same percentage of the notional
+
+    df_calc['fwd strike'].loc[0] = np.interp(days_mat_new_position[dm1].days,
+                                             df_daycount_dc.loc[dm1].astype(float).values,
+                                             df_forwards.loc[dm1].astype(float).values)
+
+    # mark to market the forwards that we are holding
+    df_calc['fwd mtm'] = np.interp(df_calc['fwd day count'].astype(float).values,
+                                   df_daycount_dc.loc[d].astype(float).values,
+                                   df_forwards.loc[d].astype(float).values)
+
+    # pnl from the positions, excluding the one that was just closed out
+    previous_pnl = df_calc['pnl'].iloc[:-1].sum()
+
+    # pnl for the current forwards
+    if currency in ['JPY']:
+        df_calc['pnl'] = - df_calc['notionals'] * (df_calc['fwd mtm'] - df_calc['fwd strike']) / df_calc['fwd mtm']
+    else:
+        df_calc['pnl'] = df_calc['notionals'] * (df_calc['fwd mtm']/df_calc['fwd strike'] - 1)
+
+
+"""
+* test JPY
+"""
