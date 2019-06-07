cbaab07a9eb45a00ecfb31a25452d77f4bf374f3
Gustavo Amarante <gusamarante@gmail.com>
2019-06-06 22:05:11 -0300

    modifications to HRP example

--- portfolio/hrp_teste.py
+++ portfolio/hrp_teste.py
@@ -4,21 +4,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def generate_data(n_obs, size0, size1, sigma1):
-    x = np.random.normal(0, 1, size=(n_obs, size0))
-    cols = [np.random.randint(0, size0-1) for i in range(size1)]
-    y = x[:, cols] + np.random.normal(0, sigma1, size=(n_obs, len(cols)))
-    x = np.append(x, y, axis=1)
-    df = pd.DataFrame(data=x, columns=['r' + str(r) for r in range(1, x.shape[1] + 1)])
-    return df, cols
+def generate_mnorm(n_var, n_obs):
+    mu = np.zeros(n_var)
+    sd = np.random.uniform(1, 3, n_var)
+    corr = np.random.uniform(-1, 1, (n_var, n_var))
+
+    for ii in range(corr.shape[0]):
+        corr[ii, ii] = 1
+
+    covar = np.diag(sd).dot(corr).dot(np.diag(sd))
+
+    data = np.random.multivariate_normal(mu, covar, n_obs)
+
+    df = pd.DataFrame(data=data,
+                      columns=['r' + str(i) for i in range(data.shape[1])])
+    return df, corr
 
 
-df, cols = generate_data(100, 3, 2, 0.5)
+df_simul, corr_true = generate_mnorm(5, 1000)
 
-hrp = HRP(data=df)
+hrp = HRP(data=df_simul)
 print(hrp.link)
+print(corr_true)
 
-# labels = list(df.columns)
+# labels = list(df_simul.columns)
 # plt.pcolor(hrp.corr)
 # plt.colorbar()
 # plt.yticks(np.arange(.5, hrp.corr.shape[0]+.5), labels)
@@ -35,6 +44,3 @@ print(hrp.link)
 
 hrp.plot_corr_matrix()
 hrp.plot_dendrogram()
-
-
-
