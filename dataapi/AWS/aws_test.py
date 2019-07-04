5aa461c24528827ac89c3e81ee144c7d1438c7dc
Gustavo Amarante <gusamarante@gmail.com>
2019-07-04 15:58:13 -0200

    HRP Class

--- dataapi/AWS/aws_test.py
+++ dataapi/AWS/aws_test.py
@@ -18,7 +18,23 @@ db_connect = create_engine("{flavor}://{username}:{password}@{host}:{port}/{data
                                    port=connect_dict['port'],
                                    flavor=connect_dict['flavor']))
 
+# ===== Examples =====
 tf = TrackerFeeder(db_connect)
 
+# Fetch the full metadata table (useful for filtering)
 df = tf.fetch_metadata()
 print(df)
+
+# fetch specific tickers
+df = tf.fetch(['eqs br itub4', 'eqs us jpm'])
+print(df)
+
+# fetch assets with certain characteristics
+filter_dict = {'sector': ['industrial', 'financial'],
+               'country': 'BR'}
+df = tf.filter_fetch(filter_dict, ret='series')
+print(df)
+
+# grab metadata possibilities
+param_dict = tf.filter_parameters()
+print(param_dict)
--- dataapi/AWS/getawsdata.py
+++ dataapi/AWS/getawsdata.py
@@ -2,12 +2,23 @@ import pandas as pd
 
 
 class TrackerFeeder(object):
-    # TODO usuario pode passar db_connect ou connect dict
+    """
+    Feeder for the trackers of the FinanceHub database.
+    """
 
     def __init__(self, db_connect):
+        """
+        Feeder construction
+        :param db_connect: sql connection engine from sqlalchemy
+        """
         self.conn = db_connect
 
     def fetch(self, fh_ticker):
+        """
+        grabs trackers from the FH database
+        :param fh_ticker: str or list with the tickers from the database trackers
+        :return: pandas DataFrame with tickers on the columns
+        """
 
         assert type(fh_ticker) is str or type(fh_ticker) is list or type(fh_ticker) is dict, \
             "'tickers' must be a string, list or dict"
@@ -32,11 +43,68 @@ class TrackerFeeder(object):
             df = df.rename(fh_ticker, axis=1)
 
         df.index = pd.to_datetime(df.index)
+        df = df.dropna(how='all')
         df = df.sort_index()
 
         return df
 
     def fetch_metadata(self):
+        """
+        Returns the full metadata table of the FH trackers, which is useful to do custom filters and look at what
+        is in the database.
+        :return: pandas Dataframe
+        """
         sql_query = 'SELECT * FROM "trackers_description"'
         df = pd.read_sql(sql=sql_query, con=self.conn)
         return df
+
+    def filter_fetch(self, filter_dict, ret='series'):
+        """
+        Grabs the trackers from the FH database that satisfy the criteria given by 'filter_dict'.
+        :param filter_dict: dict. Keys must be column names from the metadata table. Values must be
+                            either str or list of str
+        :param ret: If 'series', returns the a dataframe with the tracker series that staistfy the conditions.
+                    If 'tickers', returns a list of the tickers that staistfy the conditions.
+        :return: list or pandas DataFrame
+        """
+
+        assert type(filter_dict) is dict, "'filter_dict' must be a dict"
+        assert len(filter_dict) > 0, "'filter_dict' is empty"
+        assert ret.lower() in ['series', 'tickers'], "'ret' must be either 'series' or 'ticker'"
+
+        desc_query = 'SELECT fh_ticker FROM trackers_description WHERE '
+
+        for col in filter_dict.keys():
+
+            if type(filter_dict[col]) is list:
+                desc_query = desc_query + col + " IN ('" + "', '".join(filter_dict[col]) + "')"
+            else:
+                desc_query = desc_query + col + f" IN ('{filter_dict[col]}')"
+
+            desc_query = desc_query + ' and '
+
+        desc_query = desc_query[:-5]
+        df = pd.read_sql(sql=desc_query, con=self.conn)
+
+        tickers = df.values.flatten().tolist()
+
+        if ret == 'tickers':
+            return tickers
+
+        df = self.fetch(tickers)
+        return df
+
+    def filter_parameters(self):
+        """
+        Grabs the possible columns and their respective unique values from the metadata table.
+        :return: dict. Keys are the column names, values are list of unique values of the column.
+        """
+
+        df = self.fetch_metadata()
+
+        param_dict = {}
+
+        for col in df.columns:
+            param_dict[col] = df[col].unique().tolist()
+
+        return param_dict
