19cd423076c3e4b19a06a91b6079d313f0037736
gsoares <gustavo.soares@bwgi.com.br>
2019-07-18 19:43:03 -0200

    fixing __init__ file for comm tracker

--- trackers/__init__.py
+++ trackers/__init__.py
@@ -1,5 +1,6 @@
 from trackers.BondFutures.bondfuturetracker import BondFutureTracker
 from trackers.SingleNameEquity.singlenameequity import SingleNameEquity
 from trackers.FX.fx_tracker import FXForwardTrackers
+from trackers.Commodities.comm_futures_tracker import CommFutureTracker
 
-__all__ = ['BondFutureTracker', 'SingleNameEquity', 'FXForwardTrackers']
+__all__ = ['BondFutureTracker', 'SingleNameEquity', 'FXForwardTrackers','CommFutureTracker']
