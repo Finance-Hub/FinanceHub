d4a0fc942005a382e2ae7f2562201dea705feb26
Gustavo Amarante <gusamarante@gmail.com>
2020-03-05 11:07:24 -0300

    finished the VNA of NTN-B

--- trackers/GovBonds/Brazil/ntnbvna.py
+++ trackers/GovBonds/Brazil/ntnbvna.py
@@ -5,10 +5,10 @@ from calendars import DayCounts
 dc = DayCounts('BUS/252', calendar='anbima')
 
 # BW path
-# file_path = r'C:\Users\gamarante\Dropbox\Aulas\Insper - Financas Quantitativas\VNA Raw.xlsx'
+file_path = r'C:\Users\gamarante\Dropbox\Aulas\Insper - Financas Quantitativas\VNA Raw.xlsx'
 
 # macbook path
-file_path = r'/Users/gusamarante/Dropbox/Aulas/Insper - Financas Quantitativas/VNA Raw.xlsx'
+# file_path = r'/Users/gusamarante/Dropbox/Aulas/Insper - Financas Quantitativas/VNA Raw.xlsx'
 
 df_mensal = pd.read_excel(file_path, 'Mensal', index_col=0)
 df_diario = pd.read_excel(file_path, 'Diario', index_col=0, na_values=['#N/A N/A'])
@@ -17,7 +17,7 @@ df_release.columns = ['Date', 'IPCA']
 
 df = pd.DataFrame(index=pd.date_range('2003-03-18', 'today', freq='D'),
                   columns=['dia util', 'ultima virada', 'DU desde virada', 'DU entre viradas', 'time fraction',
-                           'proj anbima', 'saiu IPCA', 'ultimo IPCA', 'proj IPCA', 'VNA'])
+                           'proj anbima', 'saiu IPCA', 'ultimo IPCA', 'proj IPCA', 'ultimo index', 'VNA'])
 df.index.name = 'Date'
 
 df['dia util'] = dc.isbus(df.index)
@@ -45,7 +45,7 @@ for d, dm1 in tqdm(zip(df.index[1:], df.index[:-1])):
     if d.day <= 15 and df.loc[dm1, 'saiu IPCA']:
         df.loc[d, 'saiu IPCA'] = True
     else:
-        df.loc[d, 'saiu IPCA'] = df_release.index.isin([d]).any()
+        df.loc[d, 'saiu IPCA'] = df_release['Date'].isin([d]).any()
 
 # fill 'ultimo IPCA' column
 df_aux = df.index.to_frame()
@@ -56,7 +56,17 @@ df['ultimo IPCA'] = df_aux['IPCA']/100
 
 df['proj IPCA'] = df['saiu IPCA']*df['ultimo IPCA'] + (1 - df['saiu IPCA'])*df['proj anbima']
 
-# TODO VNA
+# Ultimo Index do IPCA
+df_aux = df['ultima virada'].to_frame('Dates')
+df_aux['Dates'] = pd.to_datetime(df_aux['Dates'])
+df_aux = pd.merge_asof(df_aux, df_mensal.reset_index()[['Dates', 'IPCA Index']])
+df_aux.index = df.index
+df['ultimo index'] = df_aux['IPCA Index']
 
+# VNA
+df['VNA'] = 1000*(df['ultimo index']/1614.62)*((1+df['proj IPCA'])**df['time fraction'])
+
+# drop non business days
+df = df[df['dia util']]
 
 df.to_clipboard()
