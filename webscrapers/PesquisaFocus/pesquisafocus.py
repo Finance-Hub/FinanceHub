a54724a04f308e853c5aacd2cad2d29c0a5b1844
Gustavo Amarante <gusamarante@gmail.com>
2021-03-18 18:54:19 -0300

    minor progress on FocusIPCA

--- webscrapers/PesquisaFocus/pesquisafocus.py
+++ webscrapers/PesquisaFocus/pesquisafocus.py
@@ -1,3 +1,9 @@
+
+
 class FocusIPCA(object):
+    """
+    Classe para puxar os dados de IPCA do focus.
+    - Selenium Webdriver
+    """
 
     pass
--- webscrapers/__init__.py
+++ webscrapers/__init__.py
@@ -1,4 +1,5 @@
 from webscrapers.B3derivatives.b3scraper import ScraperB3Derivatives
 from webscrapers.CETIP.getcetipdata import CETIP
+from webscrapers.PesquisaFocus.pesquisafocus import FocusIPCA
 
-__all__ = ['ScraperB3Derivatives', 'CETIP']
+__all__ = ['ScraperB3Derivatives', 'CETIP', 'FocusIPCA']
