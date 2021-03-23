b0f7726f11ca9d28b269d20dbb9a6fbe921ffe29
matheus-cordesilva <matheus.cordesilva@gmail.com>
2021-03-23 19:15:49 -0300

    - 'file path' não está sendo definido no código
    - no site do sgs pede data que foi feita  a previsão e data para qual foi feita a previsão

--- webscrapers/PesquisaFocus/pesquisafocus.py
+++ webscrapers/PesquisaFocus/pesquisafocus.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import datetime as dt
 import pandas as pd
 import os
+import time as time
 
 
 class FocusIPCA(object):
@@ -14,9 +15,8 @@ class FocusIPCA(object):
         self.driver_options = webdriver.ChromeOptions()
 
     def scrape(self, initial_date, end_date):
-
-        # Chrome WebDriver is created
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome("C:/Users/mathe/Downloads/chromedriver_win32/chromedriver.exe",
+                                   chrome_options=self.driver_options)
 
         # navigating to the page
         browser.get("https://www3.bcb.gov.br/expectativas/publico/consulta/serieestatisticas")
@@ -37,88 +37,96 @@ class FocusIPCA(object):
         xpath = r'//*[@id="periodicidade"]/option[2]'
         browser.find_element_by_xpath(xpath).click()
 
-        # dates in string format
-        if type(initial_date) is str:
-            initial_date = pd.to_datetime(initial_date).strftime('%m/%d/%Y')
-        else:
-            initial_date = initial_date.strftime('%m/%d/%Y')
+        # dates in datetime format
+        initial_date = pd.to_datetime(initial_date)
+        end_date = pd.to_datetime(end_date)
 
-        # dates in string format
-        if type(end_date) is str:
-            end_date = pd.to_datetime(end_date).strftime('%m/%d/%Y')
-        else:
-            end_date = end_date.strftime('%m/%d/%Y')
+        aux_initial_date = initial_date  # to grab the data each 2 year
+        list_df = []  # to concat all the df at the end
+        grab = True  # to stop the while
+        while grab:
+            if end_date.year - initial_date.year >= 2:
+                aux_end_date = aux_initial_date + dt.timedelta(days=729)  # adjust the end date
+                more_than_2years = 1  # identify if the range is more than 2 years
 
-        # there is a limit of two years to grab the data
-        while end_date.year - initial_date.year >= 2:
-            initial_date = pd.to_datetime(initial_date)
-            end_date = pd.to_datetime(end_date)
+                # it means the time range is less than two years after some loops
+                # that's why we adjust the aux_end_date to grab the rest of the data between initial_date and end_date
+                if aux_end_date >= end_date:
+                    aux_end_date = end_date  # adjust the end date
+                    grab = False  # final loop
+            else:
+                grab = False  # in this case, the first is the final loop
+                more_than_2years = 0  # identify if the range is less than 2 years
+                aux_end_date = end_date
 
-            aux_end_date = dt.date(initial_date.day, initial_date.month, initial_date.year + 2)
+            # fill the dates
+            xpath = r'//*[@id="tfDataInicial1"]'
+            browser.find_element_by_xpath(xpath).send_keys(aux_initial_date.strftime('%m/%d/%Y'))
 
-            'same as above'
+            # fill the dates
+            xpath = r'//*[@id="tfDataFinal2"]'
+            browser.find_element_by_xpath(xpath).send_keys(aux_end_date.strftime('%m/%d/%Y'))
+
+            # fill initial month
+            xpath = r'//*[@id="mesReferenciaInicial"]/option[{a}]'.format(a=aux_initial_date.month + 1)
+            browser.find_element_by_xpath(xpath).click()
+
+            # fill initial year
+            xpath = r'//*[@id="form4"]/div[2]/table/tbody[3]/tr/td[2]/select[2]/option[{a}]'.format(a=aux_initial_date.year - 1997)
+            browser.find_element_by_xpath(xpath).click()
+
+            # fill final month
+            xpath = r'//*[@id="mesReferenciaFinal"]/option[{a}]'.format(a=aux_end_date.month)
+            browser.find_element_by_xpath(xpath).click()
+
+            # fill final year
+            xpath = r'//*[@id="form4"]/div[2]/table/tbody[3]/tr/td[4]/select[2]/option[{a}]'.format(
+                a=aux_end_date.year - 1997)
+            browser.find_element_by_xpath(xpath).click()
+
+            # click the download button
+            xpath = r'//*[@id="btnXLSa"]'
+            browser.find_element_by_xpath(xpath).click()
+
+            # give some time for the download to finish
+            time.sleep(6)
+
+            # saves the time the file was downloaded
+            download_save_time = dt.datetime.now()
+
+            # get the default download directory
+            username = os.getlogin()
+            download_path = r'C:/Users/{}/Downloads'.format(username)
+
+            for (dirpath, dirnames, filenames) in os.walk(download_path):
+
+                for f in filenames:
+
+                    if 'Séries de estatísticas' in f:
+                        file_save_time = os.path.getmtime(dirpath + '\\' + f)
+                        file_save_time = dt.datetime.fromtimestamp(file_save_time)
+
+                        if file_save_time > download_save_time:
+                            file_path = dirpath + '\\' + f
+
+            # read the file and clean the dataframe
+            df = pd.read_excel(file_path, skiprows=1, na_values=[' '])
+
+            df['Data'] = pd.to_datetime(df['Data'], dayfirst=True)
+            df = df.set_index('Data')
 
             df = pd.read_excel(file_path, skiprows=1, na_values=[' '])
 
             df['Data'] = pd.to_datetime(df['Data'], dayfirst=True)
             df = df.set_index('Data')
 
-            list_df = list_df.append(df)
+            if more_than_2years == 1:
+                list_df.append(df)  # df's that will be concatenated
+                aux_initial_date = aux_end_date + dt.timedelta(days=1)  # adjust the initial date
 
-            initial_date.year = initial_date.year + 2
+                if not grab:
+                    df = pd.concant(list_df)  # concatenate all the data grabbed
 
+        browser.close()
 
-        # fill the dates
-        xpath = r'//*[@id="tfDataInicial7"]'
-        browser.find_element_by_xpath(xpath).send_keys(initial_date)
-
-        # fill the dates
-        xpath = r'//*[@id="tfDataFinal8"]'
-        browser.find_element_by_xpath(xpath).send_keys(end_date)
-
-        # fill initial month
-        initial_month = pd.to_datetime(initial_date).month
-        xpath = r'//*[@id="mesReferenciaInicial"]/option[{}]'.format(initial_month)
-        browser.find_element_by_xpath(xpath).click()
-
-        # fill initial year
-        initial_year = pd.to_datetime(initial_date).year
-        xpath = r'//*[@id="form7"]/div[2]/table/tbody[3]/tr/td[2]/select[2]/option[{}]'.format(initial_year)
-        browser.find_element_by_xpath(xpath).click()
-
-        # fill final month
-        end_month = pd.to_datetime(initial_date).month
-        xpath = r'//*[@id="mesReferenciaFinal"]/option[{}]'.format(end_month)
-        browser.find_element_by_xpath(xpath).click()
-
-        # fill final year
-        end_year = pd.to_datetime(initial_date).year
-        xpath = r'//*[@id="form7"]/div[2]/table/tbody[3]/tr/td[4]/select[2]/option[{}]'.format(end_year)
-        browser.find_element_by_xpath(xpath).click()
-
-        # click the download button
-        xpath = r'//*[@id="btnXLSd"]'
-        browser.find_element_by_xpath(xpath).click()
-
-        # saves the time the file was downloaded
-        download_save_time = dt.datetime.now()
-
-        # get the default download directory
-        username = os.getlogin()
-        download_path = r'C:\Users\%(user)s\Downloads' % {'user': username}
-
-        for (dirpath, dirnames, filenames) in os.walk(download_path):
-            for f in filenames:
-                if 'Séries de estatísticas' in f:
-                    file_save_time = os.path.getmtime(dirpath + '\\' + f)
-                    file_save_time = dt.datetime.fromtimestamp(file_save_time)
-
-                    if file_save_time > download_save_time:
-                        file_path = dirpath + '\\' + f
-
-        # read the file and clean the dataframe
-        df = pd.read_excel(file_path, skiprows=1, na_values=[' '])
-
-        df['Data'] = pd.to_datetime(df['Data'], dayfirst=True)
-        df = df.set_index('Data')
-        pass
+        return df
