87caa720d77157100d94adb44124adb3826269ed
Lucas Lancellotti Sanches <Lucas@Lucas.mit.edu>
2021-04-02 22:03:42 -0300

    Added NTNB class in government_bonds

--- finmath/brazilian_bonds/government_bonds.py
+++ finmath/brazilian_bonds/government_bonds.py
@@ -162,3 +162,109 @@ class NTNF(object):
         convexity = (convexity / self.price) / (1. + self.rate) ** 2
 
         return mod_duration, convexity
+
+
+class NTNB(object):
+
+    def __init__(self,
+                 expiry: Date,
+                 rate: Optional[float] = None,
+                 price: Optional[float] = None,
+                 updated_nominal_value: Optional[float] = None,
+                 principal: float = 1e6,
+                 coupon_rate: float = 0.06,
+                 ref_date: Date = TODAY):
+        """
+        Class constructor.
+        This is a Brazilian inflation-linked government bond that pays coupons every six months
+        :param expiry: bond expiry date
+        :param rate: bond yield
+        :param price: bond price
+        :param updated_nominal_value: bond nominal value updated by inflation
+        :param principal: bond principal
+        :param coupon_rate: bond coupon rate
+        :param ref_date: reference date for price or rate calculation
+        """
+
+        msg = 'Two of the parameters rate, price and updated_nominal_value must be not None!'
+        assert (rate is not None and price is not None)\
+               or (rate is not None and updated_nominal_value is not None)\
+               or (price is not None and updated_nominal_value is not None), msg
+
+        self.expiry = pd.to_datetime(expiry).date()
+        self.ref_date = pd.to_datetime(ref_date).date()
+
+        if updated_nominal_value is None:
+            self.price = float(price)
+            self.rate = float(rate)
+            self.updated_nominal_value = principal * self.updated_nominal_value_from_price(principal, coupon_rate)
+        else:
+            self.updated_nominal_value = float(updated_nominal_value)
+
+        interest = ((1. + coupon_rate) ** (1. / 2.) - 1.) * self.updated_nominal_value
+        cash_flows = pd.Series(index=self.payment_dates(),
+                               data=interest).sort_index()
+        cash_flows.iloc[-1] += self.updated_nominal_value
+
+        self.cash_flows = cash_flows
+
+        if price is None:
+            self.rate = float(rate)
+            self.price = self.price_from_rate()
+        elif rate is None:
+            self.price = float(price)
+            self.rate = self.rate_from_price()
+
+        self.mod_duration, self.convexity = self.calculate_risk
+        self.macaulay = self.mod_duration * (1. + self.rate)
+        self.dv01 = (self.mod_duration / 100.) * self.price
+
+    def payment_dates(self):
+        payd = [dc.following(self.expiry)]
+        d = dc.workday(dc.eom(dc.following(self.expiry), offset=-7), 1)
+        while d > dc.following(self.ref_date):
+            payd += [d]
+            d = dc.workday(dc.eom(d, offset=-7), 1)
+
+        return sorted(payd)
+
+    def price_from_rate(self) -> float:
+        pv = 0.
+        for d, p in self.cash_flows.items():
+            cf = LTN(d, rate=self.rate, principal=p,
+                     ref_date=self.ref_date)
+            pv += cf.price
+        return float(pv)
+
+    def rate_from_price(self):
+        theor_p = lambda x: sum([LTN(d, rate=x, principal=p,
+                                     ref_date=self.ref_date).price
+                                 for d, p in self.cash_flows.items()])
+        error = lambda x: (self.price - float(theor_p(x)))
+
+        return optimize.brentq(error, 0., 1.)
+
+    def updated_nominal_value_from_price(self, principal, coupon_rate):
+        interest = lambda x: ((1. + coupon_rate) ** (1. / 2.) - 1.) * principal * x
+        last_pay = lambda x: principal * x + interest(x)
+        theor_p = lambda x: LTN(dc.following(self.expiry), rate=self.rate, principal=last_pay(x), ref_date=self.ref_date).price +\
+                            sum([LTN(d, rate=self.rate, principal=interest(x), ref_date=self.ref_date).price
+                                 for d in self.payment_dates() if d != dc.following(self.expiry)])
+        error = lambda x: (self.price - float(theor_p(x)))
+
+        return optimize.brentq(error, 0., 1.)
+
+    @property
+    def calculate_risk(self):
+        macaulay = 0.
+        convexity = 0.
+        for d, p in self.cash_flows.items():
+            pv = p / (1. + self.rate) ** dc.tf(self.ref_date, d)
+            t = dc.tf(self.ref_date, d)
+            macaulay += t * pv
+            convexity += t * (1 + t) * pv
+        macaulay = macaulay / self.price
+        mod_duration = macaulay / (1. + self.rate)
+        convexity = (convexity / self.price) / (1. + self.rate) ** 2
+
+        return mod_duration, convexity
