f4e5d07b61ac6fd7614852acc4c475cdf67c3a4f
Gustavo Amarante <gusamarante@gmail.com>
2021-04-08 21:31:30 -0300

    added chart to visualize the results

--- webscrapers/focus_teste.py
+++ webscrapers/focus_teste.py
@@ -1,8 +1,9 @@
 from webscrapers import FocusIPCA
+import matplotlib.pyplot as plt
 
 focus = FocusIPCA(r'/Users/gustavoamarante/Desktop/chromedriver')
 
 df = focus.scrape('2018-01-01', '2021-12-31')
 
-print(df)
-
+df.plot()
+plt.show()
