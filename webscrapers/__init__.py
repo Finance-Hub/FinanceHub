e4689bccb3e108f0deb58c771f00a2fccb7ece3b
gusamarante <Jmba@160959>
2021-05-14 17:39:01 -0300

    Correcting the init files

--- webscrapers/__init__.py
+++ webscrapers/__init__.py
@@ -1,5 +1,5 @@
 from webscrapers.B3derivatives.b3scraper import ScraperB3Derivatives
 from webscrapers.CETIP.getcetipdata import CETIP
-from webscrapers.PesquisaFocus.pesquisafocus import FocusIPCA
+from webscrapers.PesquisaFocus.pesquisafocus import Focus
 
-__all__ = ['ScraperB3Derivatives', 'CETIP', 'FocusIPCA']
+__all__ = ['ScraperB3Derivatives', 'CETIP', 'Focus']
--- webscrapers/focus_teste.py
+++ webscrapers/focus_teste.py
@@ -1,13 +1,12 @@
 from webscrapers import Focus
 import matplotlib.pyplot as plt
 
-# focus = FocusIPCA(r'/Users/gustavoamarante/Desktop/chromedriver')
-focus = Focus(r'C:/Users/mathe/Downloads/chromedriver_win32/chromedriver.exe')
+focus = Focus(r'/Users/gusamarante/Desktop/chromedriver')
+# focus = Focus(r'C:/Users/mathe/Downloads/chromedriver_win32/chromedriver.exe')
 
 df = focus.scrape(indicator='pib', initial_date='2018-01-01', end_date='2021-12-31', frequency='quarterly')
 
+# TODO upload df to database
+
 df.plot()
 plt.show()
-a = 2
-
-
