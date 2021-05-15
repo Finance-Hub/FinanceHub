e09312d961ba8b8cab0fbd89bb307347753b31b1
Gustavo Amarante <gusamarante@gmail.com>
2021-05-15 15:44:53 -0300

    Added a feeder for focus data and an example for its functionalities

--- dataapi/__init__.py
+++ dataapi/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ['SGS', 'FRED', 'IMF', 'TrackerFeeder', 'DBConnect', 'DI1']
+__all__ = ['SGS', 'FRED', 'IMF', 'TrackerFeeder', 'DBConnect', 'DI1', 'FocusFeeder']
 
 from dataapi.SGS.getsgsdata import SGS
 from dataapi.FRED.getfreddata import FRED
 from dataapi.IMF.getimfdata import IMF
-from dataapi.AWS.getawsdata import TrackerFeeder
+from dataapi.AWS.getawsdata import TrackerFeeder, FocusFeeder
 from dataapi.AWS.getb3derivatives import DI1
 from dataapi.AWS.dbutils import DBConnect
