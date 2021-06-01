c546ace98ee470656eb2b05d3328cc9287c5f7e0
Andre <andre.trofino@gmail.com>
2021-05-31 23:20:26 -0300

    Add accrual method for each currency

--- finmath/RateUtils/RateUtils.py
+++ finmath/RateUtils/RateUtils.py
@@ -6,32 +6,65 @@ from calendars import DayCounts
 
 class RateUtils:
 
-    accrual_methods = ['BUS', 'ACT']
+    accrual_methods = ['BUS/252', 'ACT/360', 'ACT/365']
 
-    def __init__(self, dc='BUS/252', calendar='anbima'):
-        self.dc = DayCounts(dc, calendar=calendar)
+    def __init__(self, calendar='anbima'):
+        self.currency_map = {
+            'BRL': {
+                'method': self.__bus_252,
+                'shift': 0,
+            },
+            'USD': {
+                'method': self.__act_360,
+                'shift': 1,
+            },
+            'CHF': {
+                'method': self.__act_360,
+                'shift': -1,
+            },
+            'EUR': {
+                'method': self.__act_360,
+                'shift': 0,
+            },
+            'GBP': {
+                'method': self.__act_365,
+                'shift': 0,
+            },
+            'JPY': {
+                'method': self.__act_365,
+                'shift': 1,
+            },
+        }
+        self.calendar = calendar
         self.accrual_methods_map = {
-            'BUS': self.__accrual_bus,
-            'ACT': self.__accrual_act
+            'BUS/252': self.__bus_252,
+            'ACT/360': self.__act_360,
+            'ACT/365': self.__act_365
         }
 
-    @staticmethod
-    def __accrual(rates):
-        return (1 + rates).cumprod()
+    def __act_360(self, rates, dates, shift):
+        dc = DayCounts(dc='ACT/360', calendar=self.calendar)
+        rates = rates.shift(shift)
+        n_days = [dc.days(t0, t1) for t0, t1 in zip(dates[1:], dates[:-1])]
+        accrual = n_days * (rates/100)/360
+        return (1 + accrual).cumprod()
 
-    def __accrual_act(self, rates, dates):
-        n_days = [self.dc.days(t0, t1) for t0, t1 in zip(dates[1:], dates[:-1])]
+    def __act_365(self, rates, dates, shift):
+        dc = DayCounts(dc='ACT/365', calendar=self.calendar)
+        rates = rates.shift(shift)
+        n_days = [dc.days(t0, t1) for t0, t1 in zip(dates[1:], dates[:-1])]
         accrual = n_days * (rates/100)/365
-        return self.__accrual(accrual)
+        return (1 + accrual).cumprod()
 
-    def __accrual_bus(self, rates, dates):
+    def __bus_252(self, rates, *_):
         return ((1 + rates/100)**(1/252)).cumprod()
 
-    def accrual(self, rates: pd.Series, dates: pd.Series, method: str):
+    def currency_accrual(self, rates: pd.Series, dates: pd.Series, currency: str):
         try:
-            return self.accrual_methods_map[method](rates, dates)
+            curr = self.currency_map[currency]
+            return curr['method'](rates, dates, curr['shift'])
         except KeyError:
-            print(f'Accrual method: {method} is not supported! Supported values: {RateUtils.accrual_methods}')
+            print(f'Currency: {currency} is not supported! Supported values: {self.currency_map.keys()}')
             raise
 
 
@@ -41,5 +74,5 @@ if __name__ == "__main__":
     cdi = cdi[::-1].reset_index()
     utils = RateUtils()
 
-    acc = utils.accrual(cdi['CDI (taxa)'], cdi['Date'], 'BUS')
+    acc = utils.currency_accrual(cdi['CDI (taxa)'], cdi['Date'], 'BRL')
     print(acc)
